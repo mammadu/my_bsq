1aea0e2f4255fc74519753947014cb75e9fd0a7a
jairo guzman <guzman_j@qwasar.io>
2021-06-21 23:36:22 +0000

    Make map command generates map.txt files for reading purposes in my_bsq

---
+++ Makefile
@@ -0,0 +1,26 @@
+CC = gcc
+PL = perl
+CFLAGS += -Wall -Wextra -g3 #-Werror
+CFLAGS_MEM = ${CFLAGS} -fsanitize=address
+NAME = my_bsq
+MAP_GENERATOR = map_generator.pl
+MAP_DESTINATION = map.txt
+AGRV0 = 10
+ARGV1 = 10
+ARGV2 = 3
+
+all: my_bsq
+
+${NAME}: my_bsq.o my_c_functions.o
+	${CC} -o $@ $^ ${CFLAGS}
+
+%.o: %.c %.h
+	${CC} ${CFLAGS} -c $< 
+
+clean:
+	rm -f *.o core.* 
+
+map:
+	 ${PL} ${MAP_GENERATOR} ${AGRV0} ${ARGV1} ${ARGV2} > ${MAP_DESTINATION}
+
+re: clean ${NAME}
---
+++ my_c_functions.h
@@ -0,0 +1,50 @@
+#ifndef my_c_functions_H
+#define my_c_functions_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+char* my_strdup(char* param_1);
+
+char* combine_strings(char* first_string, char* second_string);
+
+int my_strlen(char* string);
+
+int my_recursive_pow(int base, int exponent);
+
+int my_atoi_base(char* str, int str_base);
+
+void my_putstr(char* param_1);
+
+char* reverse_string(char* param_1);
+
+char hex_to_char (int num, char base);
+
+char* my_itoa_base(int value, int base);
+
+int my_strcmp(char* param_1, char* param_2);
+
+void null_filler(void* pointer, int length);
+
+void split_string_word(char* destination_string, char** source_string, char delimiter);
+
+int delimiter_count(char* source_string, char delimiter);
+
+char** split_string(char* og_source_string, char delimiter);
+
+void free_string_array(char** string_array, int string_count);
+
+int space_validator(char* source_string);
+
+void my_putchar(char c);
+
+void my_putnbr(int nb);
+
+int my_isdigit(int c);
+
+int my_str_is_numeric(char* str);
+
+int nbr_inbase(char c, int base);
+
+
+#endif
