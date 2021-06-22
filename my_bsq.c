fed40d1a4a455f1ad4594dd618219839371b8e35
jairo guzman <guzman_j@qwasar.io>
2021-06-22 00:21:28 +0000

    opening file before checking if we can officialy do that

---
+++ my_bsq.c
@@ -0,0 +1,17 @@
+#include "my_c_functions.h"
+#include "my_bsq.h"
+
+int open_map(char* file_name)
+{
+    int fd = open(); 
+}
+
+
+int main(int argc, char* argc[])
+{   
+    if (argc < 2 && argc > 3)
+        return -1;
+    
+
+    return 0;
+}
---
+++ my_bsq.h
@@ -0,0 +1,5 @@
+#ifndef my_bsq_H
+#define my_bsq_H
+
+ #include <sys/stat.h>
+#include <fcntl.h>
